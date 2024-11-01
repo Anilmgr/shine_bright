2ab566e00b39242954b3d2b6c4cf7d9be3f29689
Anilmgr <anilcodes60@gmail.com>
2024-11-01 15:34:20 +1100

    setups

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout } from "./pages/HomeLayout";
+import { Landing } from "./pages/Landing";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+            },
+        ],
+    },
+]);
+
+const App = () => {
+    return <RouterProvider router={router} />;
+};
+
+export default App;
