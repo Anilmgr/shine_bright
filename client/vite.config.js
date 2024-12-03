bb27add767fd0c3cf98a3c0d34fe6d481b657faf
prabeshrimal <10303@ait.nsw.edu.au>
2024-12-03 15:38:07 +1100

    fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
       proxy: {
           "/api": {
-              target: "https://shine-bright-api.onrender.com",
+              target: "https://shine-bright-api.onrender.com/api",
               changeOrigin: true,
               rewrite: (path) => path.replace(/^\/api/, ""),
           },
